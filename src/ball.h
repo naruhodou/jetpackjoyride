fdca27ce6ce1444ad8084a6681fb3e879eff412c
Rohan Bhandari <raudraroop27@gmail.com>
2019-01-16 22:45:53 +0530

    added boilerplate code

---
+++ src/ball.h
@@ -0,0 +1,21 @@
+#include "main.h"
+
+#ifndef BALL_H
+#define BALL_H
+#define GLM_ENABLE_EXPERIMENTAL
+class Ball {
+public:
+    Ball() {}
+    Ball(float x, float y, color_t color);
+    glm::vec3 position;
+    float rotation;
+    void draw(glm::mat4 VP);
+    void set_position(float x, float y);
+    void tick(int sn);
+    double speed;
+    bool moveY = false;
+private:
+    VAO *object;
+};
+
+#endif // BALL_H
